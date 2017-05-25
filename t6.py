e6df0be2174057b63fbf3a8e594b493df449b289
ipower <ipowerink@outlook.com>
2017-05-25 22:48:41 +0800

    定义一个添加层的函数,修正错误

--- t6.py
+++ t6.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))  # 设置行列
-    biases = tf.Varable(tf.zeros([1, out_size]) + 0.1)  # 设置偏执不为零
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)  # 设置偏执不为零
 
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
 
